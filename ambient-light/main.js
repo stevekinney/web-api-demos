85430b93a823baa24f34944c56e36929b57ddccb
Steve Kinney <hello@stevekinney.net>
2015-04-04 14:31:46 -0600

    Clean up ambient light demo; add base page

--- ambient-light/main.js
+++ ambient-light/main.js
@@ -1,9 +1,14 @@
-var luxReading = document.querySelector('.lux');
+(function () {
+  'use strict';
 
-window.addEventListener('devicelight', function(event) {
-  var lux = event.value;
-  luxReading.innerHTML = lux;
+  var luxReading = document.querySelector('.lux');
 
-  document.body.style.backgroundColor = `rgb(${lux}, ${lux}, ${lux})`;
-  document.body.style.color = `rgb(${255 - lux}, ${255 - lux}, ${255 - lux})`
-});
+  window.addEventListener('devicelight', function(event) {
+    var lux = event.value;
+    luxReading.innerHTML = lux;
+
+    document.body.style.backgroundColor = `rgb(${lux}, ${lux}, ${lux})`;
+    document.body.style.color = `rgb(${255 - lux}, ${255 - lux}, ${255 - lux})`;
+  });
+
+}());
