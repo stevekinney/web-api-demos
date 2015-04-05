5eea369cf583666a33a91cc3ccf8cde4cc5fcc9c
Steve Kinney <hello@stevekinney.net>
2015-04-05 09:58:10 -0600

    Refactor WebSocket demonstration

--- public/websockets/main.js
+++ public/websockets/main.js
@@ -1,7 +1,9 @@
 /* globals io */
 
+// Initialize the WebSocket connection.
 var socket = io();
 
+// Grab some DOM element that we're going to be using throughout the application.
 var author = document.getElementById('author');
 var message = document.getElementById('message');
 var submitButton = document.getElementById('submit');
@@ -9,6 +11,7 @@ var submitButton = document.getElementById('submit');
 var messages = document.querySelector('.messages');
 var statusMessage = document.querySelector('.status');
 
+// Send a message over the WebSocket when the form is submitted.
 submitButton.addEventListener('click', function (event) {
   event.preventDefault();
   socket.send('message', {
@@ -17,8 +20,9 @@ submitButton.addEventListener('click', function (event) {
   });
 });
 
+// Handle received message from the WebSocket server.
 socket.on('new message', addMessageToPage);
-socket.on('new connection', updateStatus.bind(null, 'Someone has disconnected.'));
+socket.on('new connection', updateStatus.bind(null, 'A new user has connected.'));
 socket.on('lost connection', updateStatus.bind(null, 'Someone has disconnected.'));
 
 // Utility functions to modifying the page when an event is received over the
@@ -26,11 +30,12 @@ socket.on('lost connection', updateStatus.bind(null, 'Someone has disconnected.'
 function addMessageToPage(data) {
   var newMessage = document.createElement('div');
   newMessage.className = 'message';
-  newMessage.innerHTML = `<p><strong>${data.author}</strong>: ${data.message}</p>`
+  newMessage.innerHTML = `<p><strong>${data.author}</strong>: ${data.message}</p>`;
   messages.appendChild(newMessage);
 }
 
-function updateStatus(message, data) {
+function updateStatus(connectionMessage, data) {
   var users = data.connections === 1 ? 'user' : 'users';
-  statusMessage.textContent = `${message} There are now ${data.connections} ${users}.`;
+  statusMessage.textContent = `${connectionMessage}
+                               There are now ${data.connections} ${users}.`;
 }
